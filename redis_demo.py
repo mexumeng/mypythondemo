477a51c3627d2e6aa47928239adc1fdb574dc84f
Administrator <599597576@qq.com>
2018-09-04 23:20:32 +0800

    Initial commit

---
+++ redis_demo.py
@@ -0,0 +1,12 @@
+__author__ = "xumeng"
+__date__ = "2018/9/4 22:06"
+
+import redis
+
+
+r = redis.Redis(host='127.0.0.1', port=6379)
+r.set("name", '66666')
+print(r.get('name'))
+r.mset('id''1', 'cd', '2')
+print(r.mget('id', 'cd'))
+
